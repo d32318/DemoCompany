4c0360005339e2e6341ebca7ce3e67e6f9dcf46c
David <syuanru.jhu@iisigroup.com>
2020-05-09 23:53:54 +0800

    JDBCTemplate sql demo projects__v0.1

--- demo/demo/src/main/java/com/organization/demo/service/DepartmentService.java
+++ demo/demo/src/main/java/com/organization/demo/service/DepartmentService.java
@@ -13,20 +13,14 @@ public class DepartmentService {
 	private DepartmentDao departmentDao;
 	
 	public String AddDepartment(DepartmentDomain request) {
-		// TODO Auto-generated constructor stub
-		
 		return departmentDao.addDepartment(request);
 	}
 
 	public void DelDepartment(int number) {
-		// TODO Auto-generated method stub
-		
 		departmentDao.delDepartment(number);
 	}
 
 	public String ModDepartment(DepartmentDomain request) {
-		// TODO Auto-generated method stub
-		
 		return departmentDao.modDepartment(request);
 	}
 }
--- demo/demo/src/main/java/com/organization/demo/service/EmployeeService.java
+++ demo/demo/src/main/java/com/organization/demo/service/EmployeeService.java
@@ -8,30 +8,23 @@ import com.organization.demo.repository.EmployeeDao;
 @Service
 public class EmployeeService {
 
+	
 	@Autowired
 	private EmployeeDao employeeDao;
 	
 	public String AddEmployee(EmployeeDomain request) {
-		// TODO Auto-generated constructor stub
-		
 		return employeeDao.addEmployee(request);
 	}
 
 	public void DelEmployee(int number) {
-		// TODO Auto-generated method stub
-		
 		employeeDao.delEmployee(number);
 	}
 
 	public String ModEmployee(EmployeeDomain request) {
-		// TODO Auto-generated method stub
-		
 		return employeeDao.modEmployee(request);
 	}
-
+	
 	public String SearchEmployee(EmployeeDomain request) {
-		// TODO Auto-generated method stub
-		
 		return employeeDao.searchEmployee(request);
 	}
 
